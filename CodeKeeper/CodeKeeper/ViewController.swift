c9bdd400211688858a46db887b375483610ecb33
AlexYanski <yanski.job@gmail.com>
2017-02-23 23:48:52 +0300

    began my Graduate Thesis (alpha v0.0.3)

--- CodeKeeper/CodeKeeper/ViewController.swift
+++ CodeKeeper/CodeKeeper/ViewController.swift
@@ -15,8 +15,15 @@ class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
     @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var menuButtonSecond: UIButton!
     @IBOutlet weak var menuButtonThird: UIButton!
+    @IBOutlet weak var popButton: UIButton!
+    
+    var firstButtonCenter: CGPoint!
+    var secondButtonCenter: CGPoint!
+    var thirdButtonCenter: CGPoint!
+
     
     let transition = CircularTransition()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +31,36 @@ class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
         menuButton.layer.cornerRadius = menuButton.frame.size.width / 2
         menuButtonSecond.layer.cornerRadius = menuButtonSecond.frame.size.width / 2
         menuButtonThird.layer.cornerRadius = menuButtonThird.frame.size.width / 2
+        popButton.layer.cornerRadius = popButton.frame.size.width / 2
 
+        firstButtonCenter = menuButton.center
+        secondButtonCenter = menuButtonSecond.center
+        thirdButtonCenter = menuButtonThird.center
+
+        
+        menuButton.center = popButton.center
+        menuButtonSecond.center = popButton.center
+        menuButtonThird.center = popButton.center
+    }
+    
+    @IBAction func popButtonOC(_ sender: UIButton) {
+        if sender.currentImage == #imageLiteral(resourceName: "moreOn") {
+            sender.setImage(#imageLiteral(resourceName: "moreOff"), for: .normal)
+            UIView.animate(withDuration: 0.3, animations: {
+                self.menuButton.center = self.firstButtonCenter
+                self.menuButtonSecond.center = self.secondButtonCenter
+                self.menuButtonThird.center = self.thirdButtonCenter
+
+            })
+        } else {
+            sender.setImage(#imageLiteral(resourceName: "moreOn"), for: .normal)
+            UIView.animate(withDuration: 0.3, animations: {
+                self.menuButton.center = self.popButton.center
+                self.menuButtonSecond.center = self.popButton.center
+                self.menuButtonThird.center = self.popButton.center
+                
+            })
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -73,16 +109,7 @@ class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
     
     
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//        if menuButton.isTouchInside {
-//            transition.transitionMode = .dismiss
-//            transition.startingPoint = menuButton.center
-//            transition.circleColor = menuButton.backgroundColor!
-//        }
-//        if menuButtonSecond.isTouchInside {
-//            transition.transitionMode = .dismiss
-//            transition.startingPoint = menuButtonSecond.center
-//            transition.circleColor = menuButtonSecond.backgroundColor!
-//        }
+
         if temp == 2 {
             transition.transitionMode = .dismiss
             transition.startingPoint = menuButtonThird.center

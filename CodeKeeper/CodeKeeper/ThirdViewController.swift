98a9172cbc9b0f78e04d60fb98b759a6f22534ec
AlexYanski <yanski.job@gmail.com>
2017-02-23 17:17:56 +0300

    began my Graduate Thesis (alpha v0.0.2)

--- CodeKeeper/CodeKeeper/ThirdViewController.swift
+++ CodeKeeper/CodeKeeper/ThirdViewController.swift
@@ -1,27 +1,26 @@
 //
-//  SecondViewController.swift
+//  ThirdViewController.swift
 //  CodeKeeper
 //
-//  Created by Alexandr Yanski on 22.02.17.
+//  Created by Alexandr Yanski on 23.02.17.
 //  Copyright © 2017 Alexandr Yanski. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ThirdViewController: UIViewController {
 
-    @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var dismissButtonSecond: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        dismissButton.layer.cornerRadius = dismissButton.frame.size.width / 2;
-        
+        dismissButtonSecond.layer.cornerRadius = dismissButtonSecond.frame.size.width / 2;
+
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func dismissSecondVC(_ sender: Any) {
-        
+    @IBAction func dismissThirdVC(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     override func didReceiveMemoryWarning() {
--- CodeKeeper/CodeKeeper/ViewController.swift
+++ CodeKeeper/CodeKeeper/ViewController.swift
@@ -13,35 +13,91 @@ class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
     
     @IBOutlet weak var bkgImage: UIImageView!
     @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var menuButtonSecond: UIButton!
+    @IBOutlet weak var menuButtonThird: UIButton!
     
     let transition = CircularTransition()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        menuButton.layer.cornerRadius = menuButton.frame.size.width / 2;
-        
+        menuButton.layer.cornerRadius = menuButton.frame.size.width / 2
+        menuButtonSecond.layer.cornerRadius = menuButtonSecond.frame.size.width / 2
+        menuButtonThird.layer.cornerRadius = menuButtonThird.frame.size.width / 2
+
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let secondVC = segue.destination as! SecondViewController
-        secondVC.transitioningDelegate = self
-        secondVC.modalPresentationStyle = .custom
+        if menuButton.isTouchInside {
+            let secondVC = segue.destination as! SecondViewController
+            secondVC.transitioningDelegate = self
+            secondVC.modalPresentationStyle = .custom
+
+        }
+        if menuButtonSecond.isTouchInside {
+            let thirdVC = segue.destination as! ThirdViewController
+            thirdVC.transitioningDelegate = self
+            thirdVC.modalPresentationStyle = .custom
+        }
+        if menuButtonThird.isTouchInside {
+            let forthVC = segue.destination as! ForthViewController
+            forthVC.transitioningDelegate = self
+            forthVC.modalPresentationStyle = .custom
+        }
+        
     }
     
+    var temp = 0
+    
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.transitionMode = .present
-        transition.startingPoint = menuButton.center
-        transition.circleColor = menuButton.backgroundColor!
-        
+        if menuButton.isTouchInside {
+            transition.transitionMode = .present
+            transition.startingPoint = menuButton.center
+            transition.circleColor = menuButton.backgroundColor!
+            temp = 1
+        }
+        if menuButtonSecond.isTouchInside {
+            transition.transitionMode = .present
+            transition.startingPoint = menuButtonSecond.center
+            transition.circleColor = menuButtonSecond.backgroundColor!
+            temp = 0
+        }
+        if menuButtonThird.isTouchInside {
+            transition.transitionMode = .present
+            transition.startingPoint = menuButtonThird.center
+            transition.circleColor = menuButtonThird.backgroundColor!
+            temp = 2
+        }
         return transition
     }
     
+    
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.transitionMode = .dismiss
-        transition.startingPoint = menuButton.center
-        transition.circleColor = menuButton.backgroundColor!
-        
+//        if menuButton.isTouchInside {
+//            transition.transitionMode = .dismiss
+//            transition.startingPoint = menuButton.center
+//            transition.circleColor = menuButton.backgroundColor!
+//        }
+//        if menuButtonSecond.isTouchInside {
+//            transition.transitionMode = .dismiss
+//            transition.startingPoint = menuButtonSecond.center
+//            transition.circleColor = menuButtonSecond.backgroundColor!
+//        }
+        if temp == 2 {
+            transition.transitionMode = .dismiss
+            transition.startingPoint = menuButtonThird.center
+            transition.circleColor = menuButtonThird.backgroundColor!
+        }
+        if temp == 1 {
+            transition.transitionMode = .dismiss
+            transition.startingPoint = menuButton.center
+            transition.circleColor = menuButton.backgroundColor!
+        }
+        if temp == 0 {
+            transition.transitionMode = .dismiss
+            transition.startingPoint = menuButtonSecond.center
+            transition.circleColor = menuButtonSecond.backgroundColor!
+        }
         return transition
     }
 

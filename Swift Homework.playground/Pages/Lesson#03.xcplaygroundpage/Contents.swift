f5529dd12e32a8ba6b361e9fc33f860adb737348
AlexYanski <yanski.job@gmail.com>
2017-08-15 18:30:03 +0300

    Homework Day #02

--- Swift Homework.playground/Pages/Lesson#03.xcplaygroundpage/Contents.swift
+++ Swift Homework.playground/Pages/Lesson#03.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
-//: [Previous](@previous)
 
-import Foundation
 
-var str = "Hello, playground"
+// Array, Tupule, Dictionary
 
-//: [Next](@next)
+//Tupule
+
+var randomList = (123, 123.0123, "String", true)
+randomList.0
+randomList.1
+randomList.2
+randomList.3
+
+
+//Array
+
+var friendList = ["Dima","Petya","Ivan"]
+
+friendList.append("Bob")
+print(friendList)
+
+friendList.remove(at: 0)
+print(friendList)
+
+friendList.count
+friendList.removeAll()
+
+//Dictionary
+
+var myInfo = ["Hair Color":"Brown", "Height":"195", "Eye Color":"Gray"]
+
+myInfo["Height"]
+
+myInfo.updateValue("10", forKey: "Shoe Size")
+print(myInfo)
+
